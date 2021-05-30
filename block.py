7f2d5eabe57b691c39504a793426b678cd2c8f0e
Bhavy <bhavy2908@gmail.com>
2021-05-30 23:21:09 +0530

    Added Block

---
+++ block.py
@@ -0,0 +1,8 @@
+import hashlib
+
+class Block:
+    def __init__(self, previous, transaction):
+        self.transactions = transaction
+        self.previous = previous
+        string_to_hash = "".join(transaction) + previous
+        self.block_hash = hashlib.sha256(string_to_hash.encode()).hexdigest()
